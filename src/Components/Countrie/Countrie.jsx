30d2329a87da25b5a3d8c1e39ff83bb97f6a04ca
ratul-sardar <ratulsardar4747@gmail.com>
2026-03-15 23:45:12 +0600

    added some stuff

--- src/Components/Countrie/Countrie.jsx
+++ src/Components/Countrie/Countrie.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Countrie = ({ country }) => {
+const Countrie = ({ country, handleVisitedCountry }) => {
   const {
     name: { common: countryName },
     flags: {
@@ -8,14 +8,27 @@ const Countrie = ({ country }) => {
     },
     population: { population },
   } = country;
-  console.log(country);
+
+  let [state, setState] = useState(false);
+
+  function handleVisited() {
+    setState(!state);
+    handleVisitedCountry(country);
+  }
+
   return (
-    <div className="rounded-lg p-4 shadow-md bg-amber-200 space-y-2">
+    <div
+      className={`rounded-lg p-4 shadow-md ${state ? "bg-amber-200" : "bg-amber-400"}  space-y-2`}
+    >
       <img src={png} alt={alt} />
 
       <p className="text-2xl text-base-content">Name: {countryName}</p>
 
       <p className="text-2xl text-base-content">Population: {population}</p>
+
+      <button className="btn btn-accent text-white" onClick={handleVisited}>
+        {state ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,18 +1,32 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Countrie from "../Countrie/Countrie";
 
 const Countries = ({ getData }) => {
   let loadData = use(getData);
   let countriesData = loadData.countries;
 
+  let [visited, setVisitedCountry] = useState([]);
+
+  function handleVisitedCountry(country) {
+    console.log("visited clicked");
+    console.log(country);
+    let countArray = [...visited, country];
+    setVisitedCountry(countArray);
+  }
+
   return (
     <>
-      <h1 className="font-semibold text-5xl mb-4">
-        This is the countries title
-      </h1>
+      <h1 className="font-semibold text-5xl mb-4">These are the countries</h1>
+      <h3 className="font-semibold text-3xl mb-4">
+        Country visited: {visited.length}
+      </h3>
       <div className="grid grid-cols-3 gap-4">
         {countriesData.map((country) => (
-          <Countrie key={country.cca3.cca3} country={country}></Countrie>
+          <Countrie
+            key={country.cca3.cca3}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+          ></Countrie>
         ))}
       </div>
     </>
